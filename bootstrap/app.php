eebcb3eb9d923cadd5d90d6dab74f3f3686da3c3
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2025-12-04 07:21:27 -0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,6 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (NotFoundHttpException $e) {
+            return response()->view('client.errors.404', [], 404);
+        });
         $exceptions->respond(function (Response $response) {
             if ($response->getStatusCode() === 419) {
                 return back()->with([
